e7d75cf798b6a41ee3862e561d67662f54c96bb6
Marquez77 <62211625+Marquez77@users.noreply.github.com>
2022-07-24 13:17:00 +0900

    Added some descriptions.

--- src/main/java/me/marquez/asmountapi/ASMountAPI.java
+++ src/main/java/me/marquez/asmountapi/ASMountAPI.java
@@ -127,6 +127,12 @@ public class ASMountAPI extends JavaPlugin {
         sendPacketsToPlayers(players, removeEntitiesPacket);
     }
 
+    /**
+     * 플레이어에게 새로운 아머스탠드를 추가합니다.
+     *
+     * @param player 대상 플레이어
+     * @param data 아머스탠드 데이터
+     */
     public void addArmorStandMount(Player player, ASMountData data) {
         Location location = player.getLocation();
         ArmorStand armorStand = new ArmorStand(((CraftWorld)location.getWorld()).getHandle(), location.getX(), location.getY(), location.getZ());
@@ -143,10 +149,23 @@ public class ASMountAPI extends JavaPlugin {
         spawnArmorStands(player, players, data);
     }
 
+    /**
+     * 플레이어로부터 아머스탠드를 제거합니다.
+     * 데이터의 이름만을 비교하여 추가되어 있는 데이터를 제거합니다.
+     *
+     * @param player 대상 플레이어
+     * @param data 제거할 아머스탠드 데이터
+     */
     public void removeArmorStandMount(Player player, ASMountData data) {
         removeArmorStandMount(player, data.getName());
     }
 
+    /**
+     * 플레이어로부터 아머스탠드를 제거합니다.
+     *
+     * @param player 대상 플레이어
+     * @param name 제거할 아머스탠드 이름
+     */
     public void removeArmorStandMount(Player player, String name) {
         if(playerMountData.containsKey(player)) {
             Map<String, ASMountData> mountData = playerMountData.get(player);
@@ -157,6 +176,12 @@ public class ASMountAPI extends JavaPlugin {
         }
     }
 
+    /**
+     * 플레이어의 아머스탠드를 보이지 않도록 합니다.
+     *
+     * @param player 대상 플레이어
+     * @param viewer 보이지 않게 할 플레이어
+     */
     public void hideArmorStandMounts(Player player, Player viewer) {
         excludeView.add(Pair.of(player, viewer));
         if(playerMountData.containsKey(player)) {
@@ -164,7 +189,13 @@ public class ASMountAPI extends JavaPlugin {
         }
     }
 
-    public void showArtifact(Player player, Player viewer) {
+    /**
+     * 플레이어의 아머스탠드를 보이게 합니다.
+     *
+     * @param player 대상 플레이어
+     * @param viewer 보이게 할 플레이어
+     */
+    public void showArmorStandMounts(Player player, Player viewer) {
         excludeView.remove(Pair.of(player, viewer));
         if(playerMountData.containsKey(player)) {
             spawnArmorStands(player, Collections.singletonList(viewer), playerMountData.get(player).values().toArray(new ASMountData[0]));
